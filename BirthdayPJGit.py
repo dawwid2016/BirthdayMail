3942cbed5657d9e342ae69e5a4dd26f2c394a704
David Crazz <62417504+dawwid2016@users.noreply.github.com>
2021-01-20 11:11:22 +0800

    Update BirthdayPJGit.py

--- BirthdayPJGit.py
+++ BirthdayPJGit.py
@@ -8,6 +8,7 @@
 模块功能：读取当前文件夹里的Excel文件，发送某旦学邮
 """
 
+# 导入所需包
 import pandas as pd
 import datetime
 from selenium import webdriver
@@ -19,6 +20,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+# 从excel文件中读取姓名-生日-邮箱信息
 def readList():
     bookDir = './BirthdayList.xlsx'
     df = pd.read_excel(bookDir, sheet_name='Sheet1')
@@ -28,42 +30,49 @@ def readList():
     else:
         return 0, df
 
+# 发送到草稿箱
 def send(df):
     #Open the Mail Website
     chrome_driver = './chromedriver.exe'
     driver = webdriver.Chrome(executable_path = chrome_driver)
     driver.get("https://mail.fudan.edu.cn/")
+    # 登录
     time.sleep(2)
-    driver.find_element_by_id("uid").click()
-    driver.find_element_by_id("uid").clear()
+    driver.find_element_by_id("uid").click()    # 点选账号栏
+    driver.find_element_by_id("uid").clear()    # 清空账号栏
     studID = '1630068XXXX' #change to your student id
-    driver.find_element_by_id("uid").send_keys("%s")
-    driver.find_element_by_id("password").click()
-    driver.find_element_by_id("password").clear()
-    driver.find_element_by_id("password").send_keys("PASSWD")
-    driver.find_element_by_xpath('//*[@id="logArea"]/div[6]/button').click()
+    driver.find_element_by_id("uid").send_keys("%s")    # 输入账号
+    driver.find_element_by_id("password").click()    # 点选密码栏
+    driver.find_element_by_id("password").clear()    # 清空密码栏
+    driver.find_element_by_id("password").send_keys("PASSWD")    # 输入密码
+    driver.find_element_by_xpath('//*[@id="logArea"]/div[6]/button').click()  # 登录按钮
     time.sleep(2)
+    # 找到发送按钮并进入发送界面
     driver.find_element_by_xpath('//*[@id="navContainer"]/div[1]/a[2]').click()
     time.sleep(2)
+    # 进入写信框架
     driver.switch_to.frame("compose1")
     # 读取列表
     nrows = df.shape[0]
     count = 0
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # 计数器控制发送
     while count < nrows:        
+        # 提取姓名-生日-邮箱
         name = df.iloc[count, 0]
         birthday = df.iloc[count, 1]
-        mailAddress = df.iloc[count, 2]       
+        mailAddress = df.iloc[count, 2]     
+        # 编辑标题和内容
         title = "Happy Birthday!"
         content = "Hi %s,\n     Happy Birthday to you! \n XXX office"%(name)
-        # Receiver
+        # 收件人填写
         address = driver.find_element_by_id("InputEx_To")
         ActionChains(driver).move_to_element(address).send_keys(mailAddress).send_keys(Keys.RETURN).perform()
-        # Title
+        # 标题填写
         driver.find_element_by_id("subject").click()
         driver.find_element_by_id("subject").clear()
         driver.find_element_by_id("subject").send_keys(title)
-        # content
+        # 内容填写
         driver.switch_to.frame("htmleditor")
         driver.switch_to.frame("HtmlEditor")
         text = driver.find_element_by_xpath('/html/body')
@@ -94,6 +103,7 @@ def send(df):
         time.sleep(1)
         # 点击再写一封
         count += 1
+    #完成发送，退出
     print("done")
     time.sleep(5)
     driver.close()
@@ -101,6 +111,6 @@ def send(df):
 if __name__ == '__main__':
     flag, df = readList()
     if flag == 1:
-        print("The excel file has more or less than 3 columns.")
+        print("The excel file has more or less than 3 columns.") # 格式出错的返回
     if flag == 0:
         send(df)
